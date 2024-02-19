80f85a36a764c6268d3d3d4b3e46c0f5d63cd384
程煜晴 <chengyuqing@chengyuqingdeMacBook-Pro.local>
2024-02-19 16:45:39 +0800

    rewrite match_name

--- bond/character/match_name.py
+++ bond/character/match_name.py
@@ -18,8 +18,10 @@ names_wrong = [
 ]
 
 
-# 检查是否含有中文字符
 def is_contains_chinese(strs):
+    """
+    Check if contains chinese characters.
+    """
     for _char in strs:
         if '\u4e00' <= _char <= '\u9fa5':
             return True
@@ -27,18 +29,21 @@ def is_contains_chinese(strs):
 
 
 def match_name(name, target_name):
+    """
+    Match different forms of names.
+    """
     [first_name, last_name] = target_name.split('_')
     first_name = re.sub('-', '', first_name)
-    # 中文名转化为拼音形式
+    # change Chinese name to pinyin
     if is_contains_chinese(name):
         name = re.sub('[^ \u4e00-\u9fa5]', '', name).strip()
         name = pinyin.get(name, format='strip')
-        # 两个字的人名中间可能有空格
+        # remove blank space between characters
         name = re.sub(' ', '', name)
         target_name = last_name + first_name
         return name == target_name
     else:
-        # 处理带声调的拼音
+        # unifying Pinyin characters with tones
         str_bytes = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
         name = str_bytes.decode('ascii')
 
@@ -57,7 +62,7 @@ def match_name(name, target_name):
                 tokens = tokens[:-1]
 
         if re.match(tokens[0], last_name):
-            # 中文名两个字母缩写(如果只有一个字母原方案能解决)
+            # unifying two-letter abbreviation of the Chinese name
             if len(tokens) == 2 and len(tokens[1]) == 2:
                 if re.match(f'{tokens[1][0]}.*{tokens[1][1]}.*', first_name):
                     return True
